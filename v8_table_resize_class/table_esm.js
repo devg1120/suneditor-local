339a722b85fc41b34464186b9c0e50f09ce5ebe1
devg1120 <devg1120@gmail.com>
2025-05-02 06:56:56 +0000

    up

--- v8_table_resize_class/table_esm.js
+++ v8_table_resize_class/table_esm.js
@@ -1,6 +1,4 @@
-//  document.onselectstart = function() {
-//    return false;
-//  }
+
 
 export class Table {
   col_element;
@@ -1081,3 +1079,28 @@ row_dragging_over = (e) => {
 
   }
 } // class end
+
+
+let s = SUNEDITOR.create('sample', {
+    //plugins: plugins,
+    mode: 'inline',
+    buttonList: [
+        ['undo', 'redo'],
+        ['font', 'fontSize', 'formatBlock'],
+        ['paragraphStyle', 'blockquote'],
+        ['bold', 'underline', 'italic', 'strike', 'subscript', 'superscript'],
+        ['fontColor', 'hiliteColor', 'textStyle'],
+        ['removeFormat'],
+        '/', // Line break
+        ['outdent', 'indent'],
+        ['align', 'horizontalRule', 'list', 'lineHeight'],
+        ['table', 'link', 'image', 'video', 'audio' /** ,'math' */], // You must add the 'katex' library at options to use the 'math' plugin.
+        /** ['imageGallery'] */ // You must add the "imageGalleryUrl".
+        ['fullScreen', 'showBlocks', 'codeView'],
+        ['preview', 'print'],
+        ['save', 'template'],
+        /** ['dir', 'dir_ltr', 'dir_rtl'] */ // "dir": Toggle text direction, "dir_ltr": Right to Left, "dir_rtl": Left to Right
+    ]
+})
+
+
